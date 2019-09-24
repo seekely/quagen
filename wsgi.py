50a61223451045cff5b5e3352143d43598d7713c
Ryan Seekely <ryan@happykoalas.com>
2019-09-23 18:27:43 -0700

    A pylint runner so we don't have to screw around with pylint's inability to be recursive.

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,23 @@
+"""
+WSGI driver
+"""
+
 import os
 import sys
-
-new_path = os.path.dirname(os.path.abspath(__file__)) + "/src"
-sys.path.insert(0, new_path)
-os.chdir(new_path)
-
 from quagen import create_app
 
-app = create_app()
+
+def main():
+    """
+    Creates our WSGI app
+    """
+    new_path = os.path.dirname(os.path.abspath(__file__)) + "/src"
+    sys.path.insert(0, new_path)
+    os.chdir(new_path)
+
+    app = create_app()
+    app.run()
+
 
 if __name__ == "__main__":
-    app.run()
+    main()
