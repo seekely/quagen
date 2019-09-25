c2cefadfb53189d2364c2a68e5349630a3d166d6
Ryan Seekely <ryan@happykoalas.com>
2019-09-24 20:20:07 -0700

    Turning the projection profile into an assertion test so we can see if we mess anything up too bad.

---
+++ tests/profile/quagen/test_profile_game.py
@@ -0,0 +1,35 @@
+"""
+Profile game functions
+"""
+import cProfile
+import time
+
+from quagen.game import Board
+from quagen.game import Game
+
+
+def test_profiles():
+    """
+    Run game profiles
+    """
+    print("Profiling score calculation with projection")
+    board = Board({}, Game.DEFAULT_SETTINGS)
+    board.generate()
+    board.apply_moves([[4, 19, 1], [9, 4, 2]])
+
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    start = time.time()
+    board.calculate_scores()
+    end = time.time()
+
+    profiler.disable()
+    profiler.print_stats()
+
+    total_time = end - start
+    assert total_time < 0.25
+
+
+if __name__ == "__main__":
+    test_profiles()
