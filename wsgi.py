8f0b1b010dd4e4d083c970b260ed8dda5320da4b
Ryan Seekely <ryan@happykoalas.com>
2019-09-25 17:51:03 -0700

    Switching wsgi.py back to imperfect but working version.

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,19 @@
 """
 WSGI driver
 """
+# pylint: disable=invalid-name
+# pylint: disable=wrong-import-position
 
 import os
 import sys
+
+# Add Quagen code to path
+new_path = os.path.dirname(os.path.abspath(__file__)) + "/src"
+sys.path.insert(0, new_path)
+os.chdir(new_path)
 from quagen import create_app
 
-
-def main():
-    """
-    Creates our WSGI app
-    """
-    new_path = os.path.dirname(os.path.abspath(__file__)) + "/src"
-    sys.path.insert(0, new_path)
-    os.chdir(new_path)
-
-    app = create_app()
-    app.run()
-
+app = create_app()
 
 if __name__ == "__main__":
-    main()
+    app.run()
