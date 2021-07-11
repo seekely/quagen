5af51ca2ae4312df150362bff64dbca7136e8640
Ryan Seekely <ryan@happykoalas.com>
2021-07-11 14:41:43 -0700

    It's been a while. Let's upgrade some NPM packages.

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,36 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
-
-const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: "src/quagen/ui/exports.js",
   output: {
-    sourcemap: true,
-    format: "iife",
+    file: "src/quagen/static/js/ui.js",
+    format: 'iife',
     name: "ui",
-    file: "src/quagen/static/js/ui.js"
+    sourcemap: true
+  },
+  watch: {
+    clearScreen: false
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: css => {
-        css.write("src/quagen/static/css/ui.css");
+
+      include: 'src/quagen/ui/**/*.svelte',
+      emitCss: false,
+      
+      // Svelete compiler options, 
+      // see https://svelte.dev/docs#svelte_compile
+      compilerOptions: {
+        css: css => {
+          css.write("src/quagen/static/css/ui.css");
+        }
       }
-    }),
 
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration —
-    // consult the documentation for details:
-    // https://github.com/rollup/rollup-plugin-commonjs
-    resolve({
-      browser: true,
-      dedupe: importee =>
-        importee === "svelte" || importee.startsWith("svelte/")
     }),
+    resolve({ browser: true }),
     commonjs(),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
-    production && terser()
-  ],
-  watch: {
-    clearScreen: false
-  }
-};
+    terser(),
+  ]
+}
